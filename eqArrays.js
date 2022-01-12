2ec2bfd29d6ecd94c460dc5a8a44792602fd5d54
mhakbar <habibakbar@live.com>
2022-01-11 22:34:31 -0800

    eqArrays.js created

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+const assertEqual = require("./assertEqual");//Importing assertEqual function from assertEqual.js
+
+const eqArrays = function (array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  };
+  
+
+  ////////eqArrays Function//////////
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] === array2[i]) {
+      //console.log(`🛑🛑🛑 ${array1} !== ${array2}`);
+      continue;   
+      
+    } else {
+      return false;      
+    }
+  }
+  return true;
+};
+//////////////////////////////////////
+
+
+//////Tests/////////////
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
