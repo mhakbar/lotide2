89be9d724201fdcba975d149e1ab03c26aaf915a
mhakbar <habibakbar@live.com>
2022-01-11 23:04:02 -0800

    modified the file further

--- eqArrays.js
+++ eqArrays.js
@@ -1,12 +1,12 @@
 const assertEqual = require("./assertEqual");//Importing assertEqual function from assertEqual.js
 
+
+  ////////eqArrays Function//////////
 const eqArrays = function (array1, array2) {
   if (array1.length !== array2.length) {
     return false;
   };
   
-
-  ////////eqArrays Function//////////
   for (let i = 0; i < array1.length; i++) {
     if (array1[i] === array2[i]) {
       //console.log(`🛑🛑🛑 ${array1} !== ${array2}`);
@@ -32,3 +32,7 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
 assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
+
+
+//////exporting function//////
+module.exports = eqArrays;
