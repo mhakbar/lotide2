d7f2aa9745d5ea8bc2c658c88336d72f3905fe46
mhakbar <habibakbar@live.com>
2022-01-12 15:22:22 -0800

    middle.js completed along with tests

---
+++ middle.js
@@ -0,0 +1,56 @@
+  ////////eqArrays Function//////////
+  const eqArrays = function (array1, array2) {
+    if (array1.length !== array2.length) {
+      return false;
+    };
+    
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+        //console.log(`🛑🛑🛑 ${array1} !== ${array2}`);
+        continue;   
+        
+      } else {
+        return false;      
+      }
+    }
+    return true;
+  };
+  //////////////////////////////////////
+///////asserArraysEqual Function///////////
+
+const assertArraysEqual = (array1, array2) => {
+
+  if(eqArrays(array1, array2)) {
+    return console.log(`✅✅✅ Assertion Passed: ${array1} === ${array2}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${array1} !== ${array2}`);
+  }
+};
+ /////////////////////////////////////////////// 
+
+///////////////Middle function/////////////////////
+
+const middle = array => {
+  if (array.length === 1 || array.length === 2) {
+    return [];
+  } else if (array.length % 2 === 0) {
+    return([array[(array.length / 2) - 1] , array[(array.length / 2)]]);
+  } else {
+    return [(array[Math.floor(array.length / 2)])];
+  }
+
+};
+
+//////////////////////////////////////////////////
+
+//////////////Tests///////////////////////////
+
+assertArraysEqual(middle([1, 2, 3]), [2]);
+
+assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3]));
+
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1]));
+console.log(middle([1 ,2]));
+console.log(middle([1, 2, 3]));
+console.log(middle([1, 2, 3, 4, 5]));
