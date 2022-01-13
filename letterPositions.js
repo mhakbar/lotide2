3b3a86a156c771dd92eaf035ab30e1693ec0344c
mhakbar <habibakbar@live.com>
2022-01-12 17:59:54 -0800

    letterPositions completed

---
+++ letterPositions.js
@@ -0,0 +1,18 @@
+const letterPositions = function(sentence) {
+  const results = {};
+  for (let i = 0; i < sentence.length; i++) {
+    if (sentence[i] !== " ") {
+      if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    //   //console.log(results[location]);
+    }
+    else {
+    results[sentence[i]] = [i];
+    }
+  }
+}
+  // logic to update results here
+  return console.log(results);
+};
+
+letterPositions("lighthouse in the house");
