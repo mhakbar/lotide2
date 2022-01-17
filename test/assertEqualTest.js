bb1493a8b8a7726af907d8804c1e62e17a57ab71
mhakbar <habibakbar@live.com>
2022-01-17 14:58:26 -0800

    test files added for all lotide functions

---
+++ test/assertEqualTest.js
@@ -0,0 +1,11 @@
+// test/assertEqualTest.js
+
+const assertEqual = require('../assertEqual');
+
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Lighthouse Labs");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
+assertEqual(1, "1");
