a1e67760c13f18f602c3c12b079c253c23ef7707
mhakbar <habibakbar@live.com>
2022-01-17 18:38:13 -0800

    Mocha and Chai tests added

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,14 +1,42 @@
-const assertArraysEqual = require('..//assertArraysEqual');
-const middle = require('../middle');
+// const assertArraysEqual = require('..//assertArraysEqual');
+// const middle = require('../middle');
 
-//////////////Tests///////////////////////////
+// //////////////Tests///////////////////////////
 
-assertArraysEqual(middle([1, 2, 3]), [2]);
+// assertArraysEqual(middle([1, 2, 3]), [2]);
 
-assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3]));
+// assertArraysEqual(middle([1, 2, 3, 4]), ([2, 3]));
 
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1]));
-console.log(middle([1 ,2]));
-console.log(middle([1, 2, 3]));
-console.log(middle([1, 2, 3, 4, 5]));
+// console.log(middle([1, 2, 3, 4]));
+// console.log(middle([1]));
+// console.log(middle([1 ,2]));
+// console.log(middle([1, 2, 3]));
+// console.log(middle([1, 2, 3, 4, 5]));
+
+
+
+////////////Mocha & Chai Tests/////////////use "npm test" and not "node" to run the tests
+
+
+const assert = require('chai').assert;
+const middle = require('..//middle');
+
+describe("middle", () => {
+  it("returns empty array for single input, [1]", () => {
+  assert.deepEqual(middle([1]), []);
+});
+
+it("returns empty array for two input only, [1, 2]", () => {
+  assert.deepEqual(middle([1, 2]), []);
+});
+
+it("returns single middle value for total odd elements in an array. Output: [3], input: [1, 2, 3, 4, 5]", () => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+});
+
+it("returns two middle values for even length array. Output: [3, 4]. Input: [1, 2, 3, 4, 5, 6]", () => {
+  assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+});
+
+});
+
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,10 +1,30 @@
-const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+// const tail = require('../tail');
+// const assertEqual = require('../assertEqual');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
 
-assertEqual(tail([1, 2, 4]), 4);
+// assertEqual(tail([1, 2, 4]), 4);
+
+const assert = require('chai').assert;
+const tail = require('..//tail');
+
+describe("#tail", () => {
+  it("returns 'Labs' for '['Hello', 'Lighthouse', 'Labs']'", () => {
+    assert.strictEqual(tail(["Hello", "Lighthouse", "Labs"]), 'Labs');
+
+  });
+
+  it("returns '4' for ['1', '2', '4']", () => {
+    assert.strictEqual(tail([1, 2, 4]), 4);
+  });
+
+  it("returns '4' for ['1', '2', '4']", () => {
+    assert.deepEqual(tail([1, 2, 4]), 4);
+  });
+
+});
+
