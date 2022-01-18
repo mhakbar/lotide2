1e7e24459e56e010c8b8d921aa8c87e2b5d0f510
mhakbar <habibakbar@live.com>
2022-01-17 16:30:52 -0800

    code refactored and tailtest.js created

--- tail.js
+++ tail.js
@@ -4,10 +4,12 @@ const tail = input => {
   return input[input.length - 1];
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words); // no need to capture the return value since we are not checking it
+// assertEqual(words.length, 3); // original array should still have 3 elements!
 
-assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
+// assertEqual(tail(["Hello", "Lighthouse", "Labs"]), "Labs");
 
-assertEqual(tail([1, 2, 4]), 4);
+// assertEqual(tail([1, 2, 4]), 4);
+
+module.exports = tail;
