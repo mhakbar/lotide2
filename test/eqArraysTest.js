7a90d9dd99b5caee2496cafdb06716293edc3974
mhakbar <habibakbar@live.com>
2022-01-17 16:37:26 -0800

    eqArraysTest created

---
+++ test/eqArraysTest.js
@@ -0,0 +1,14 @@
+const assertEqual = require('../assertEqual');
+const eqArrays = require('../eqArrays');
+
+//////Tests/////////////
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true);
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true);
