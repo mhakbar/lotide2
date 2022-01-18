e246163fd8a361a027665c945c7c629498b0bc1f
mhakbar <habibakbar@live.com>
2022-01-17 16:59:14 -0800

    assertArraysEqual.js refactored and assertArraysEqualTest.js created

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -13,9 +13,13 @@ const assertArraysEqual = (array1, array2) => {
   }
 };
 
-///////////////////////Tests//////////////////////////
+// ///////////////////////Tests//////////////////////////
 
-assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]); // => Assertion Passed
-assertArraysEqual(['1', '2', '3',], [1, 2]); // => Assertion Failed
-assertArraysEqual(['1', '2', '3',], ['1', '2', '3',]);
-assertArraysEqual([1, 2, 3, 4], [4, 3, 2, 1]);
+// assertArraysEqual([1, 2, 3, 4], [1, 2, 3, 4]); // => Assertion Passed
+// assertArraysEqual(['1', '2', '3',], [1, 2]); // => Assertion Failed
+// assertArraysEqual(['1', '2', '3',], ['1', '2', '3',]);
+// assertArraysEqual([1, 2, 3, 4], [4, 3, 2, 1]);
+
+
+
+module.exports = assertArraysEqual;
